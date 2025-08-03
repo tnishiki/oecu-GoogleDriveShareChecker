674a622f70ff3a6b1871aef77428f8906187b969
tnishiki <tnishiki@gmail.com>
2025-08-03 20:51:24 +0900

    Google に登録されているアカウント情報を管理するデータクラスを追加

---
+++ oecu-GoogleDriveShareChecker/Datas/AccountData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oecu_GoogleDriveShareChecker.Datas
+{
+    public class AccountData
+    {
+        public string Account { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+    }
+}
